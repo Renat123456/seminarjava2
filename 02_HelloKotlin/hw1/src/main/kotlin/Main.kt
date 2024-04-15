f2020c6048270217aa4259300666dbcc054cc7fd
Renat <alridcom@gmail.com>
2024-04-15 16:35:18 +0300

    ready homework

---
+++ 02_HelloKotlin/hw1/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+/* Программа println
+* */
+
+fun main(args: Array<String>) {
+    print("Ренат ") // нет переноса
+    println("Шаймарданов") // есть перенос
+    print("Курс ")
+    println("Андроид")
+//    println("Андроид")
+//    println("Андроид")
+
+}
